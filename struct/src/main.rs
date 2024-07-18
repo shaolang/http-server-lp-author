a8a1a8661a1aecf65df73823edda283adce02226
shaolang <shaolang@ymail.com>
2024-07-18 14:29:13 +0800

    Add language to Lang enum in struct subproject + screenshot of program run

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -4,6 +4,7 @@ enum Lang {
   Spanish,
   Chinese,
   Texan,
+  Japanese,
 }
 
 struct Greeting {
@@ -21,6 +22,8 @@ fn main() {
   let g : Greeting = Greeting { lang: Lang::Texan, message: String::from("Howdy WasmEdge!") };
   v.push(g);
   let g : Greeting = Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") };
+  v.push(g);
+  let g: Greeting = Greeting { lang: Lang::Japanese, message: String::from("こんにちは, WasmEdgeさん") };
   v.push(g);
 
   for e in v {
